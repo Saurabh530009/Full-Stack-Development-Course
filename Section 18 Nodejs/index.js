1e928877cf3de26b097de8f09a8e4f6319b3df6d
Saurabh Kumar Verma <2019uch0033@iitjammu.ac.in>
2023-03-18 22:26:15 +0530

    Nodejs Started

---
+++ Section 18 Nodejs/index.js
@@ -0,0 +1,13 @@
+var superheroes=require("superheroes");
+var supervillains = require("supervillains");
+
+var mySuperheroName=superheroes.random();
+var mySuperVillainName=supervillains.random();
+// console.log(superheroes.all);
+
+console.log(mySuperheroName);
+
+
+
+// console.log(supervillains.all);
+console.log(mySuperVillainName);
