f49c51cd57eb0f28b8b287a68feebec41effad26
Saurabh Kumar Verma <2019uch0033@iitjammu.ac.in>
2023-03-22 03:44:25 +0530

    Section 20 API started

--- Section 19 ExpressJs/my-express-server/server.js
+++ Section 19 ExpressJs/my-express-server/server.js
@@ -11,6 +11,14 @@ app.get("/",function(request,response){
 app.get("/contact", function(req,res){
     res.send("Contact me at: saurabh@gmail.com");
 });
+
+app.get("/about", function(req,res){
+    res.send("My name is Saurabh and I love to code.");
+});
+
+app.get("/hobbies", function(req,res){
+    res.send("Cricket, Volleyball");
+});
 app.listen(3000, function(){
     console.log("Server started on port 3000");
 });
